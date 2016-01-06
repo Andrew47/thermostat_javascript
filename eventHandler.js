a8ad4b876730bf8b4a62f680279e2be4db5acb36
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-06 21:15:09 +0000

    refactors

--- eventHandler.js
+++ eventHandler.js
@@ -1,40 +1,36 @@
-window.onload = function() {
-
-  alert( "welcome" );
-
   $( document ).ready(function () {
 
-    var thermostat = new Thermostat();
+    alert( "welcome" );
 
-      $("#temperature").text( thermostat.getCurrentTemperature());
-      $("#temperature").css('color', 'orange');
+    var thermostat = new Thermostat();
+    updateTemperature();
 
 
     $("#temperature_up").click( function () {
       thermostat.upButton();
-      color_generator();
-      $("#temperature").text( thermostat.getCurrentTemperature());
+      updateTemperature();
     });
 
     $("#temperature_down").click( function () {
       thermostat.downButton();
-      color_generator();
-      $("#temperature").text( thermostat.getCurrentTemperature());
+      updateTemperature();
+    });
+
+    $("#temperature_reset").click( function () {
+      thermostat.resetButton();
+      updateTemperature();
     });
 
     $("#power_saving_status").text( thermostat.isPowerSavingOn());
 
     $("#power_saving_switch").click( function () {
       thermostat.powerSavingSwitch();
-        $("#power_saving_status").text( thermostat.isPowerSavingOn());
+      $("#power_saving_status").text( thermostat.isPowerSavingOn());
     });
 
-    $("#temperature_reset").click( function () {
-      thermostat.resetButton();
-      $("#temperature").text( thermostat.getCurrentTemperature());
-    });
 
-    color_generator = function () {
+
+    function color_generator() {
       if (thermostat.displayColour() == 'low-usage') {
         $("#temperature").css('color', 'green');
       }
@@ -46,11 +42,12 @@ window.onload = function() {
       if (thermostat.displayColour() == 'high-usage') {
         $("#temperature").css('color', 'red');
       }
-    };
+    }
 
-
-
+    function updateTemperature() {
+      $('#temperature').text(thermostat.temperature);
+      // $('#temperature').attr('class', thermostat.displayColour());
+      color_generator();
+    }
 
   });
-
-};
