56c3a7ad4aeab05d1014bc78b02cbfae153abc08
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-07 15:39:50 +0000

    can find current weather for any city

--- eventHandler.js
+++ eventHandler.js
@@ -5,6 +5,7 @@
     var thermostat = new Thermostat();
 
     updateTemperature();
+    get_data ();
 
     $("#temperature_up").hover(
       function () { $('h2').css("backgroundColor", "rgb(255, 229, 255)"); },
@@ -42,5 +43,40 @@
       $('#temperature').text(thermostat.temperature);
       $('#temperature').attr('class', thermostat.energyUsage());
     }
+
+    function get_data (name) {
+
+    var city_name = name || 'London';
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name;
+    var token = "&APPID=4db7936adac8e360e07f2f6e0b70d42f";
+
+    $.ajax({
+
+    url: url + token,
+
+    type: "GET",
+
+    dataType : "json",
+
+    data: {
+      units: "metric"
+    },
+
+    success: function( json ) {
+      $( "#name" ).text( json.name );
+      $("#condition").text( json.weather[0].main + ': ' + json.weather[0].description);
+      $("#retrieved_temperature").text( Math.round(json.main.temp) + 'C');
+      $("#wind").text(json.wind.speed + ' meter/sec');
+    }
+
+  });}
+
+  $('#submit_name').click(function() {
+    get_data($('#name_input').val());
+  });
+
+
+
+
 
   });
