bcc2581aaaa3ee05771162fd6b843645b349c159
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-08 13:36:15 +0000

    changes header font

--- eventHandler.js
+++ eventHandler.js
@@ -1,78 +1,80 @@
-  $( document ).ready(function () {
+$( document ).ready(function () {
 
-    alert( "Welcome user - type your city for the current weather, then decide how to set the thermostat." );
+  alert( "Welcome user - type your city for the current weather, then decide how to set the thermostat." );
 
-    var thermostat = new Thermostat();
+  var thermostat = new Thermostat();
 
-    updateTemperature();
-    get_data ();
+  updateTemperature();
+  get_data ();
 
-    $("#temperature_up").hover(
-      function () { $('h2').css("backgroundColor", "rgb(255, 229, 255)"); },
+  $("#temperature_up").hover(
+    function () { $('h2').css("backgroundColor", "rgb(255, 229, 255)"); },
+    function () { $('h2').css("backgroundColor", "white"); });
+
+    $("#temperature_down").hover(
+      function () { $('h2').css("backgroundColor", "rgb(229, 242, 255)"); },
       function () { $('h2').css("backgroundColor", "white"); });
 
-      $("#temperature_down").hover(
-        function () { $('h2').css("backgroundColor", "rgb(229, 242, 255)"); },
-        function () { $('h2').css("backgroundColor", "white"); });
 
 
+      $("#temperature_up").click( function () {
+        thermostat.upButton();
+        updateTemperature();
+      });
 
-    $("#temperature_up").click( function () {
-      thermostat.upButton();
-      updateTemperature();
-    });
+      $("#temperature_down").click( function () {
+        thermostat.downButton();
+        updateTemperature();
+      });
 
-    $("#temperature_down").click( function () {
-      thermostat.downButton();
-      updateTemperature();
-    });
+      $("#temperature_reset").click( function () {
+        thermostat.resetButton();
+        updateTemperature();
+      });
 
-    $("#temperature_reset").click( function () {
-      thermostat.resetButton();
-      updateTemperature();
-    });
-
-    $("#power_saving_status").text( thermostat.isPowerSavingOn());
-
-    $("#power_saving_switch").click( function () {
-      thermostat.powerSavingSwitch();
       $("#power_saving_status").text( thermostat.isPowerSavingOn());
-    });
 
-    function updateTemperature() {
-      $('#temperature').text(thermostat.temperature);
-      $('#temperature').attr('class', thermostat.energyUsage());
-    }
+      $("#power_saving_switch").click( function () {
+        thermostat.powerSavingSwitch();
+        $("#power_saving_status").text( thermostat.isPowerSavingOn());
+      });
 
-    function get_data (name) {
+      function updateTemperature() {
+        $('#temperature').text(thermostat.temperature);
+        $('#temperature').attr('class', thermostat.energyUsage());
+      }
 
-    var city_name = name || 'London';
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name;
-    var token = "&APPID=4db7936adac8e360e07f2f6e0b70d42f";
+      function get_data (name) {
 
-    $.ajax({
+        var city_name = name || 'London';
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name;
+        var token = "&APPID=4db7936adac8e360e07f2f6e0b70d42f";
 
-    url: url + token,
+        $.ajax({
 
-    type: "GET",
+          url: url + token,
 
-    dataType : "json",
+          type: "GET",
 
-    data: {
-      units: "metric"
-    },
+          dataType : "json",
 
-    success: function( json ) {
-      $( "#name" ).text( json.name );
-      $("#condition").text( json.weather[0].main);
-      $("#retrieved_temperature").text( Math.round(json.main.temp) + 'C');
-      $("#wind").text(json.wind.speed + ' meter/sec');
-    }
+          data: {
+            units: "metric"
+          },
 
-  });}
+          success: function( json ) {
+            $( "#name" ).text( json.name );
+            $("#condition").text( json.weather[0].main);
+            $("#retrieved_temperature").text( Math.round(json.main.temp) + 'C');
+            $("#wind").text(json.wind.speed + ' meter/sec');
+          },
 
-  $('#submit_name').click(function() {
-    get_data($('#name_input').val());
-  });
+          failure: function () {alert('Problem loading API');}
 
-  });
+        });}
+
+        $('#submit_name').click(function() {
+          get_data($('#name_input').val());
+        });
+
+      });
